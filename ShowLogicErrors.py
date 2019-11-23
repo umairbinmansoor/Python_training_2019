6ae622d9fe01a0a12340d5574e42eca2ba494e85
umairbinmansoor <umairbinmansoor@gmail.com>
2019-11-23 21:14:19 +0500

    Code example for Logical Error

---
+++ ShowLogicErrors.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 23 21:39:52 2019
+
+@author: Umair Bin Mansoor
+"""
+
+# Convert Fahrenheit to Celcius
+
+print("Fahrenheit 35 is Celcius degree ")
+print(5 / 9 * 35 - 32)  # Correct is print(5 / 9 * (35 - 32))
